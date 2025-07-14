f1938903cad4ebfbb819c5031d0cf0f5d042f491
itsKgs <kunwargaurav.sahu25@gmail.com>
2025-07-14 22:28:08 +0530

    Updated NeuralPDE folder

--- NeuralPDE/10_mv_heat_src.jl
+++ NeuralPDE/10_mv_heat_src.jl
@@ -1,6 +1,7 @@
 using NeuralPDE, ModelingToolkit, DomainSets
 using Lux, Optimization, OptimizationOptimJL, OptimizationOptimisers
 using Plots
+using CUDA, LuxCUDA, Functors
 
 @parameters x y z t
 @variables T(..)
@@ -14,25 +15,24 @@ Dzz = Differential(z)^2
 x_min = t_min = y_min = z_min = 0.0
 x_max = 1.0
 y_max = 1.0
-z_max = 0.3
+z_max = 0.5
 t_max = 1.0
 
 k = 1.0
 ρ = 1.0
-C_p = 700.0
+C_p = 505.0
 D = 1/(ρ * C_p)
-D = 1.0
-a = 0.2
-b = 0.2
-c = 0.05
-q = 1.0
-v = 1.0
+a = 0.01
+b = 0.01
+c = 0.005
+q = 200 * 0.3
+v = 0.5
 
 
 # Initial conditions
 T0(x, y, z, t) = 300.0 
 
-Q(x, y, z, t) = ((6 * sqrt(3) * q)/(a * b * c * π * sqrt(π))) * exp(-3 * (((x^2)/(a^2)) + ((y^2)/(b^2)) + (((z - v * t)^2)/(c^2))))
+Q(x, y, z, t) = ((6 * sqrt(3) * q)/(a * b * c * π * sqrt(π))) * exp(-3 * ((((x - v * t)^2)/(a^2)) + (((y - 0.5)^2)/(b^2)) + ((z^2)/(c^2))))
 #Q(x, y, z, t) = 1000
 # 2. PDE system
 
@@ -57,21 +57,25 @@ bcs = [
     T(x, 0, z, t) ~ 300.0,
     T(x, 1, z, t) ~ 300.0,
     T(x, y, 0, t) ~ 300.0,
-    T(x, y, 0.3, t) ~ 300.0
+    T(x, y, 0.5, t) ~ 300.0
 ] 
 
 @named pdesys = PDESystem(eqs, bcs, domains, [x, y, z, t], [T(x, y, z, t)])
 
 # Neural network
 chain = Chain(
-    Dense(4, 64, tanh),
-    Dense(64, 64, tanh),
-    Dense(64, 1)
+    Dense(4, 128, tanh),
+    Dense(128, 128, tanh),
+    Dense(128, 1)
 )
+
+#@info "CUDA available?" CUDA.functional()
+#chain_gpu = fmap(x -> CUDA.adapt(CuArray, x), chain)   # CORRECT
+
 
 dx = 0.05
 dy = 0.05
-dz = 0.05
+dz = 0.025
 dt = 0.05
 strategy = GridTraining([dx, dy, dz, dt])
 
@@ -82,13 +86,16 @@ prob = discretize(pdesys, discretization)
 global  iter = 0
 callback = function (p, l)
     global iter =  iter + 1
-    println("Iteration: $iter, loss is: $l")
+    println("Iteration: $iter, loss is: $l") 
     return false
 end
 
-opt = OptimizationOptimJL.BFGS()
+opt1 = OptimizationOptimisers.Adam(0.001)
+res1 = Optimization.solve(prob, opt1; callback=callback, maxiters=1000)
 
+opt = Optim.LBFGS()
 res = Optimization.solve(prob, opt; callback, maxiters = 30)
+
 
 phi = discretization.phi
 
@@ -105,11 +112,23 @@ anim_T = @animate for t in ts
     heatmap(xs, ys, T_pred',
         xlabel="x", ylabel="y",
         title="T(x, y, z=$(round(zs, digits=2)), t=$(round(t, digits=2)))",
-        c=:thermal, clims = (300, 320), size=(800, 600) 
+        c=:thermal, size=(800, 600)
     ) 
 end
 
-gif(anim_T, "NeuralPDE/mv_heat_src/pinn_heatmap_z$(round(zs, digits=2)).gif", fps=9)
+gif(anim_T, "NeuralPDE/mv_heat_src/pinn_heatmap_z$(round(zs, digits=2)).gif", fps=2)
+
+anim_surface = @animate for t in ts
+    T_pred = [phi([x, y, zs, t], res.u)[1] for x in xs, y in ys]
+    surface(xs, ys, T_pred',
+        xlabel="x", ylabel="y", zlabel="T",
+        title="3D Surface T(x, y, z=$(round(zs, digits=2)), t=$(round(t, digits=2)))",
+        c=:thermal, size=(800, 600)
+    )
+end
+
+gif(anim_surface, "pinn_surface_z$(round(zs, digits=2)).gif", fps=1)
+
 
 zs = 0.10
 anim_T = @animate for t in ts
@@ -117,19 +136,31 @@ anim_T = @animate for t in ts
     heatmap(xs, ys, T_pred',
         xlabel="x", ylabel="y",
         title="T(x, y, z=$(round(zs, digits=2)), t=$(round(t, digits=2)))",
-        c=:thermal, clims = (300, 301), size=(800, 600) 
+        c=:thermal, size=(800, 600) 
     ) 
 end
 
 gif(anim_T, "NeuralPDE/mv_heat_src/pinn_heatmap_z$(round(zs, digits=2)).gif", fps=6)
 
+anim_surface = @animate for t in ts
+    T_pred = [phi([x, y, zs, t], res.u)[1] for x in xs, y in ys]
+    surface(xs, ys, T_pred',
+        xlabel="x", ylabel="y", zlabel="T",
+        title="3D Surface T(x, y, z=$(round(zs, digits=2)), t=$(round(t, digits=2)))",
+        c=:thermal, size=(800, 600)
+    )
+end
+
+gif(anim_surface, "pinn_surface_z$(round(zs, digits=2)).gif", fps=10)
+
+
 zs = 0.15
 anim_T = @animate for t in ts
     T_pred = [phi([x, y, zs, t], res.u)[1] for x in xs, y in ys]
-    heatmap(xs, ys, T_pred,
+    heatmap(xs, ys, T_pred',
         xlabel="x", ylabel="y",
         title="T(x, y, z=$(round(zs, digits=2)), t=$(round(t, digits=2)))",
-        c=:thermal, clims = (300, 320), size=(800, 600) 
+        c=:thermal, size=(800, 600), clims = (minimum(T_pred), maximum(T_pred)) 
     ) 
 end
 
